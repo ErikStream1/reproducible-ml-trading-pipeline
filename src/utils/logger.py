4c202b83f24cc8c85c5e0175feeec90ef3fd1ede
ErikStream1 <A01639729@exatec.tec.mx>
2026-01-29 14:01:36 -0700

    Add force=True to logging.basicConfig

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,4 +8,5 @@ def setup_logging(
         level = level,
         format = "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
         datefmt = "%Y-%m-%d %H:%M:%S",
+        force = True
     )
