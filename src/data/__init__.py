b596032bdd5d92518456825feb62a4177d1f8119
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-04 12:27:46 -0700

    chore(data): update __init__ imports and __all__ exports

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -12,19 +12,26 @@ from .quotes.quotes_resolver import(load_quotes,
 
 from .quotes.quotes_store import (to_utc,
                                   QuoteStore)
+
+from .quotes.collect_quotes import (_collect_ticker_rest,
+                                   collect_quotes)
+
 from .load_data import load_btc_data
 from .validate_data import validate_btc_data
 
 __all__ = ["BitsoConfig",
            "BitsoError",
            "QuoteSnapshot",
-            "QuoteSeries",
-            "CollectQuotesConfig",
-            "_parse_dt_utc",
-            "BitsoClient",
-            "load_quotes",
-            "resolve_quotes_asof",
-            "to_utc",
-            "QuoteStore",
+           "QuoteSeries",
+           "CollectQuotesConfig",
+           "_parse_dt_utc",
+           "BitsoClient",
+           "load_quotes",
+           "resolve_quotes_asof",
+           "to_utc",
+           "QuoteStore",
+           "_collect_ticker_rest",
+           "collect_quotes",
            "load_btc_data",
-           "validate_btc_data"]
+           "validate_btc_data"
+           ]
