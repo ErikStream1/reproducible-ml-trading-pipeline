524d3397b227c36922bba3fc9bcfe733c78939d4
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-06 09:06:54 -0700

    chore(strategy): update __init__ imports and __all__ exports

--- src/strategy/__init__.py
+++ src/strategy/__init__.py
@@ -3,8 +3,11 @@ from .filters import (apply_vol_filter,
 
 from .signals import threshold_signal
 
+from .sizing import target_qty_from_fixed_notional
+
 __all__ = [
     "apply_vol_filter",
     "apply_cooldown",
     "threshold_signal",
+    "target_qty_from_fixed_notional",
     ]
