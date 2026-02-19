1d9b506df2127c64c714d48570fa11b8c44358ce
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-18 19:41:24 -0700

    chore(backtest): update __init__ imports and __all__ exports

--- src/backtest/__init__.py
+++ src/backtest/__init__.py
@@ -1,5 +1,6 @@
 from .types import (BacktestReport,
-                    LedgerResult)
+                    LedgerResult,
+                    RealtimeSimulationStepResult)
 
 from .ledger import (run_ledger,)
 
@@ -9,13 +10,20 @@ from .metrics import (returns_from_equity,
                       turnover_from_position,)
 
 from .runner import (run_backtest_threshold,)
+
+from .reporting.report_store import _persist_step_result
+from .reporting.persist_backtest_artifacts import _save_backtest_artifacts
+
 __all__ = [
     "BacktestReport",
     "LedgerResult",
+    "RealtimeSimulationStepResult",
     "run_backtest_threshold",
     "run_ledger",
     "returns_from_equity",
     "max_drawdown",
     "sharpe_ratio",
-    "turnover_from_position"
+    "turnover_from_position",
+    "_persist_step_result",
+    "_save_backtest_artifacts",
 ]
