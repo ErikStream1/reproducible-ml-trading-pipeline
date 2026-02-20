47e21ccde99608edf65956f3e3c796f2d42d986b
ErikStream1 <A01639729@exatec.tec.mx>
2026-02-20 09:55:51 -0700

    chore(execution): update __init__ imports and __all__ exports

--- src/execution/__init__.py
+++ src/execution/__init__.py
@@ -1,7 +1,8 @@
 from .types import (OrderSide, 
                     Fill,
                     PositionLike,
-                    FillVectorLike)
+                    FillVectorLike,
+                    ShadowExecutionResult)
 
 from .fills import(
     fill_price_next_close,
@@ -15,16 +16,19 @@ from .fees import fee_proportional
 
 from .simulator import simulate_fills_from_target_position
 
+from .reporting.persist_shadow_artifacts import _persist_shadow_execution_artifacts
+
 __all__ = [
            "OrderSide", 
            "Fill",
            "PositionLike",
            "FillVectorLike",
+           "ShadowExecutionResult",
            "fill_price_next_close",
            "fill_price_mid",
            "fill_price_bid_ask",
            "slippage_vol",
            "slippage_bps",
            "fee_proportional",
-           "simulate_fills_from_target_position"
-           ]
+           "simulate_fills_from_target_position",
+           "_persist_shadow_execution_artifacts"]
